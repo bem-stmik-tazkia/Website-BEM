caa463af861b682b1e3a1171c64c76e9b72839d6
HafidzAlaziz <hafidzalaziz721@gmail.com>
2026-09-15 10:46:33 +0700

    fix: hapus onLoad pada Server Component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -108,10 +108,6 @@ export default async function RootLayout({
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-          media="print"
-          onLoad={(e) => {
-            (e.currentTarget as any).media = "all";
-          }}
         />
         <script
           type="application/ld+json"
